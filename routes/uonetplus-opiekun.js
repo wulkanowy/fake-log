50c9aaa0c4a770af289a9e81333b07cea957abb4
Mikołaj Pich <m.pich@outlook.com>
2017-12-11 16:11:58 +0100

    Move data to json files

--- routes/uonetplus-opiekun.js
+++ routes/uonetplus-opiekun.js
@@ -19,14 +19,21 @@ router.get("/Default/123456/Start/Index/", function (req, res) {
 });
 
 router.get("/Default/123456/Uczen.mvc/DanePodstawowe", function (req, res) {
-  res.render("opiekun/dane", { title: "Witryna ucznia i rodzica – Dane ucznia" });
+  res.render("opiekun/dane", {
+    title: "Witryna ucznia i rodzica – Dane ucznia",
+    data: require("../data/opiekun/dane.json")
+  });
 });
 
 router.get("/Default/123456/Oceny(\.mvc|)/Wszystkie", function (req, res) {
   if (req.query.details === '2') {
-    res.render("opiekun/oceny-szczegolowy");
+    res.render("opiekun/oceny-szczegolowy", {
+      data: require("../data/opiekun/oceny-szczegolowy.json")
+    });
   } else {
-    res.render("opiekun/oceny-skrocony");
+    res.render("opiekun/oceny-skrocony", {
+      data: require("../data/opiekun/oceny-skrocony.json")
+    });
   }
 });
 
