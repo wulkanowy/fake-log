9353b73befc800c01c8e43dbe6b6cf111fac417d
Mikołaj Pich <m.pich@outlook.com>
2018-08-31 21:02:24 +0200

    Add messages

--- src/routes/uonetplus-uzytkownik.js
+++ src/routes/uonetplus-uzytkownik.js
@@ -8,4 +8,105 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/Default/Wiadomosc.mvc/GetWiadomosciOdebrane", (req, res) => {
+    let i = 0;
+    res.json({
+        "success": true,
+        "data": require("../../data/api/messages/WiadomosciOdebrane").map(item => {
+            return {
+                "Nieprzeczytana": !item.GodzinaPrzeczytania,
+                "Data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "Tresc": null,
+                "Temat": item.Tytul,
+                "NadawcaNazwa": item.Nadawca,
+                "IdWiadomosci": item.WiadomoscId,
+                "IdNadawca": item.NadawcaId,
+                "Id": ++i
+            }
+        })
+    });
+});
+
+router.get("/Default/Wiadomosc.mvc/GetWiadomosciWyslane", (req, res) => {
+    res.json({
+        "success": true,
+        "data": require("../../data/api/messages/WiadomosciWyslane").map(item => {
+            return {
+                "Data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "Temat": item.Tytul,
+                "Adresaci": item.Adresaci[0].Nazwa,
+                "Nieprzeczytane": item.Nieprzeczytane,
+                "Przeczytane": item.Przeczytane,
+                "Id": item.WiadomoscId
+            }
+        })
+    });
+});
+
+router.get("/Default/Wiadomosc.mvc/GetWiadomosciUsuniete", (req, res) => {
+    let i = 0;
+    res.json({
+        "success": true,
+        "data": require("../../data/api/messages/WiadomosciUsuniete").map(item => {
+            return {
+                "FolderWiadomosci": "1",
+                "Nieprzeczytana": !item.GodzinaPrzeczytania,
+                "Data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "Tresc": null,
+                "Temat": item.Tytul,
+                "NadawcaNazwa": item.Nadawca,
+                "IdWiadomosci": item.WiadomoscId,
+                "IdNadawca": item.NadawcaId,
+                "Id": ++i
+            }
+        })
+    });
+});
+
+router.get("/Default/NowaWiadomosc.mvc/GetJednostkiUzytkownika", (req, res) => {
+    const user = require("../../data/api/ListaUczniow")[0];
+    res.json({
+        "success": true,
+        "data": [
+            {
+                "IdJednostkaSprawozdawcza": user.IdJednostkaSprawozdawcza,
+                "Skrot": user.JednostkaSprawozdawczaSkrot,
+                "Role": [1],
+                "NazwaNadawcy": user.Imie + " " + user.Nazwisko,
+                "WychowawcaWOddzialach": [],
+                "Id": user.Id
+            }
+        ]
+    });
+});
+
+router.get("/Default/NowaWiadomosc.mvc/GetAdresaci", (req, res) => {
+    const user = require("../../data/api/ListaUczniow")[0];
+    res.json({
+        "success": true,
+        "data": require("../../data/api/dictionaries/Pracownicy").map(item => {
+            return {
+                "Id": item.Id + "rPracownik",
+                "Nazwa": item.Imie + " " + item.Nazwisko + " [" + item.Kod + "] - pracownik (" + user.JednostkaSprawozdawczaSkrot + ")",
+                "IdLogin": item.Id,
+                "IdJednostkaSprawozdawcza": user.IdJednostkaSprawozdawcza,
+                "RolaEnum": null,
+                "Rola": 2,
+                "PushWiadomosc": null
+            };
+        })
+    });
+});
+
+router.all("/Default/Wiadomosc.mvc/GetTrescWiadomosci", (req, res) => {
+    const message = require("../../data/api/messages/WiadomosciOdebrane")[0];
+    res.json({
+        "success": true,
+        "data": {
+            "Id": message.WiadomoscId,
+            "Tresc": message.Tresc
+        }
+    });
+});
+
 module.exports = router;
