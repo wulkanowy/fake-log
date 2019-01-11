94a5d76af354be3a3f273f8c471cc75897211be9
Mikołaj Pich <m.pich@outlook.com>
2019-01-11 01:29:45 +0100

    Workaround heroku ssl with env var

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,5 +1,5 @@
 function getProtocol(req) {
-    let proto = req.connection.encrypted ? 'https' : 'http';
+    let proto = req.connection.encrypted || process.env.SSL === 'true' ? 'https' : 'http';
     // only do this if you trust the proxy
     proto = req.headers['x-forwarded-proto'] || proto;
     return proto.split(/\s*,\s*/)[0];
