7526ed3c605ab8e2819183db8cebac69f513dfce
Mikołaj Pich <m.pich@outlook.com>
2019-03-02 00:38:56 +0100

    Fix colors

--- src/utils/gradeColor.js
+++ src/utils/gradeColor.js
@@ -1,9 +1,9 @@
 exports.getGradeColorByCategoryName = name => {
     switch (true) {
         case name.includes("czarny"): return "000000";
-        case name.includes("czerw"): return "ff0000";
-        case name.includes("fiol"): return "800080";
-        case name.includes("nieb"): return "0000ff";
-        case name.includes("zielony"): return "00ff00";
+        case name.includes("czerw"): return "F04C4C";
+        case name.includes("fiol"): return "B16CF1";
+        case name.includes("nieb"): return "20A4F7";
+        case name.includes("zielony"): return "6ECD07";
     }
 };
