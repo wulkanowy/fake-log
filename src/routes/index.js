76197018d6185bcb057730b4ea95b2d82234099b
Mikołaj Pich <m.pich@outlook.com>
2019-04-09 21:19:24 +0200

    Fix ssl

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const protocol = require("../utils/connection");
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', {title: 'fake-log', domain: req.get('host')});
+  res.render('index', {
+    title: 'fake-log',
+    proto: protocol(req),
+    domain: req.get('host')
+  });
 });
 
 module.exports = router;
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,7 +1,9 @@
 function getProtocol(req) {
     let proto = req.connection.encrypted || process.env.SSL === 'true' ? 'https' : 'http';
+    console.log(`proto1: ${proto}`);
     // only do this if you trust the proxy
     proto = req.headers['x-forwarded-proto'] || proto;
+    console.log(`proto2: ${proto}`);
     return proto.split(/\s*,\s*/)[0];
 }
 
