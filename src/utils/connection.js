2273751d009195f4b6dd5469ca69cee40130fd08
Mikołaj Pich <m.pich@outlook.com>
2019-04-14 19:56:19 +0200

    Inline connection util

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,4 @@
-function getProtocol(req) {
+module.exports = function(req) {
     let proto = req.connection.encrypted || process.env.SSL === 'true' ? 'https' : 'http';
     return proto.split(/\s*,\s*/)[0];
-}
-
-module.exports = getProtocol;
+};
