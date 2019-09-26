078c8f9cf9450f5f610225067edd7f367340a299
Mikołaj Pich <m.pich@outlook.com>
2019-09-26 12:20:32 +0200

    Fix uonetplus domain endpoints by change matching order

--- src/routes/uonetplus.js
+++ src/routes/uonetplus.js
@@ -2,45 +2,6 @@ const express = require('express');
 const router = express.Router();
 const protocol = require('../utils/connection');
 
-router.get("/", (req, res) => {
-    res.render("log-exception", {
-        title: "Dziennik FakeUONET+",
-        message: "Podany identyfikator klienta jest niepoprawny.",
-    });
-});
-
-router.get("/Default(/)?", (req, res) => {
-    if (req.header("Referer") || "true" === req.query.login) {
-        return res.redirect("/Default/Start.mvc/Index");
-    }
-
-    res.render("login", {
-        title: "Dziennik FakeUONET+"
-    });
-});
-
-router.all("/Default/LoginEndpoint.aspx", (req, res) => {
-    if (req.body.wa && req.body.wresult) {
-        return res.redirect("/Default/?login=true");
-    }
-
-    res.redirect(protocol(req) + "://" + req.get('host').replace("uonetplus", "cufs") + "/Default/Account/LogOn");
-});
-
-router.post("(/*)?", (req, res) => {
-    res.render("permission-error", {
-        title: "Logowanie"
-    });
-});
-
-router.get("/Default/Start.mvc/Index", (req, res) => {
-    res.render("homepage", {
-        title: "Uonet+",
-        uonetplusOpiekun: protocol(req) + "://" + req.get('host').replace("uonetplus", "uonetplus-opiekun"),
-        uonetplusUczen: protocol(req) + "://" + req.get('host').replace("uonetplus", "uonetplus-uczen")
-    });
-});
-
 router.get("/Default/Start.mvc/Endpoints", (req, res) => {
     const base = protocol(req) + "://" + req.get('host') + "/Default/Start.mvc";
     res.json({
@@ -126,6 +87,45 @@ router.all("/Default/Start.mvc/GetLastStudentLessons", (req, res) => {
         "success": false,
         "errorMessage": "Not implemented yet",
         "feedback": null
+    });
+});
+
+router.get("/", (req, res) => {
+    res.render("log-exception", {
+        title: "Dziennik FakeUONET+",
+        message: "Podany identyfikator klienta jest niepoprawny.",
+    });
+});
+
+router.get("/Default(/)?", (req, res) => {
+    if (req.header("Referer") || "true" === req.query.login) {
+        return res.redirect("/Default/Start.mvc/Index");
+    }
+
+    res.render("login", {
+        title: "Dziennik FakeUONET+"
+    });
+});
+
+router.all("/Default/LoginEndpoint.aspx", (req, res) => {
+    if (req.body.wa && req.body.wresult) {
+        return res.redirect("/Default/?login=true");
+    }
+
+    res.redirect(protocol(req) + "://" + req.get('host').replace("uonetplus", "cufs") + "/Default/Account/LogOn");
+});
+
+router.post("(/*)?", (req, res) => {
+    res.render("permission-error", {
+        title: "Logowanie"
+    });
+});
+
+router.get("/Default/Start.mvc/Index", (req, res) => {
+    res.render("homepage", {
+        title: "Uonet+",
+        uonetplusOpiekun: protocol(req) + "://" + req.get('host').replace("uonetplus", "uonetplus-opiekun"),
+        uonetplusUczen: protocol(req) + "://" + req.get('host').replace("uonetplus", "uonetplus-uczen")
     });
 });
 
