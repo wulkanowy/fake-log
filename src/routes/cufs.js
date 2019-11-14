97dd5ec3f53a29808860fea7fcbd12bc34756ba7
Mikołaj Pich <m.pich@outlook.com>
2019-11-14 20:19:26 +0100

    More symbol fixes

--- src/routes/cufs.js
+++ src/routes/cufs.js
@@ -17,17 +17,17 @@ router.get("/:symbol/Account/LogOn", (req, res) => {
 });
 
 // POST login
-router.post("/powiatwulkanowy/Account/LogOn", (req, res) => {
+router.post("/:symbol/Account/LogOn", (req, res) => {
     if ('jan@fakelog.cf' === req.body.LoginName && 'jan123' === req.body.Password) {
         res.cookie("Vulcan.CUFS.WebFrontEndCookie", "1234567891012131314151617181920212223242526+");
         res.cookie("ARR_cufs.vulcan.net.pl", "1234567891012131314151617181920212223242526272829303132333435363");
-        return res.redirect("/powiatwulkanowy/FS/LS?" +
+        return res.redirect("/" + req.param("symbol") + "/FS/LS?" +
             "wa=wsignin1.0&" +
-            "wtrealm=" + protocol(req) + "%3a%2f%2fuonetplus.fakelog.localhost%3A300%2fdemo123%2fLoginEndpoint.aspx&" +
-            "wctx=" + protocol(req) + "%3a%2f%2fuonetplus.fakelog.localhost%3A300%2fdemo123%2fLoginEndpoint.aspx");
+            "wtrealm=" + protocol(req) + "%3a%2f%2fuonetplus.fakelog.localhost%3A300%2f" + req.param("symbol") + "%2fLoginEndpoint.aspx&" +
+            "wctx=" + protocol(req) + "%3a%2f%2fuonetplus.fakelog.localhost%3A300%2f" + req.param("symbol") + "%2fLoginEndpoint.aspx");
     }
 
-    res.render("login-form", {title: "Logowanie (powiatwulkanowy)", message: "Zła nazwa użytkownika lub hasło"});
+    res.render("login-form", {title: "Logowanie (" + req.param("symbol") + ")", message: "Zła nazwa użytkownika lub hasło"});
 });
 
 router.get("/powiatwulkanowy/FS/LS", (req, res) => {
