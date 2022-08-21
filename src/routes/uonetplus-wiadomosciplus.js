e86b8ce576e9b746068da6c0890e3aad1da8edd5
Mikołaj Pich <m.pich@outlook.com>
2022-08-21 10:58:21 +0200

    Update endpoints response model

--- src/routes/uonetplus-wiadomosciplus.js
+++ src/routes/uonetplus-wiadomosciplus.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const protocol = require('../utils/connection');
-const converter = require('../utils/converter');
-const {getRandomInt} = require("../utils/api");
+const {fromString} = require('uuidv4');
 
 router.get("/", (req, res) => {
     res.render("messages");
@@ -41,32 +40,18 @@ router.get([
     res.json({
         "success": true,
         "data": require("../../data/api/messages/WiadomosciOdebrane").map(item => {
-            const recipientsNumber = getRandomInt(60, 100);
-            const readBy = getRandomInt(20, 60);
-            const unreadBy = recipientsNumber - readBy;
             return {
-                "Id": item.WiadomoscId * 2,
-                "Nieprzeczytana": !item.GodzinaPrzeczytania,
-                "Nieprzeczytane": unreadBy,
-                "Przeczytane": readBy,
-                "Data": converter.formatDate(new Date(item.DataWyslaniaUnixEpoch * 1000), true) + ' 00:00:00',
-                "Tresc": null,
-                "Temat": item.Tytul,
-                "Nadawca": {
-                    "Id": "" + item.NadawcaId,
-                    "Name": item.Nadawca,
-                    "IdLogin": item.NadawcaId,
-                    "Unreaded": false,
-                    "Date": null,
-                    "Role": 2,
-                    "PushMessage": false,
-                    "UnitId": 0,
-                    "Hash": "abcdef="
-                },
-                "IdWiadomosci": item.WiadomoscId,
-                "HasZalaczniki": true,
-                "FolderWiadomosci": 1,
-                "Adresaci": []
+                "apiGlobalKey": fromString(item.WiadomoscId.toString()),
+                "korespondenci": item.Nadawca + " - P - (123456)",
+                "temat": item.Tytul,
+                "data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "skrzynka": "Jan Kowalski - U - (123456)",
+                "hasZalaczniki": true,
+                "przeczytana": !!item.GodzinaPrzeczytania,
+                "nieprzeczytanePrzeczytanePrzez": null,
+                "wazna": false,
+                "uzytkownikRola": 2,
+                "id": item.WiadomoscId
             };
         })
     });
@@ -80,28 +65,17 @@ router.get([
         "success": true,
         "data": require("../../data/api/messages/WiadomosciWyslane").map(item => {
             return {
-                "Id": item.WiadomoscId * 2,
-                "Nieprzeczytana": !item.GodzinaPrzeczytania,
-                "Nieprzeczytane": parseInt(item.Nieprzeczytane, 10),
-                "Przeczytane": parseInt(item.Przeczytane, 10),
-                "Data": converter.formatDate(new Date(item.DataWyslaniaUnixEpoch * 1000), true) + ' 00:00:00',
-                "Tresc": null,
-                "Temat": item.Tytul,
-                "Nadawca": {
-                    "Id": "" + item.NadawcaId,
-                    "Name": item.Nadawca,
-                    "IdLogin": item.NadawcaId,
-                    "Unreaded": false,
-                    "Date": null,
-                    "Role": 2,
-                    "PushMessage": false,
-                    "UnitId": 0,
-                    "Hash": "abcdef="
-                },
-                "IdWiadomosci": item.WiadomoscId,
-                "HasZalaczniki": false,
-                "FolderWiadomosci": 2,
-                "Adresaci": []
+                "apiGlobalKey": fromString(item.WiadomoscId.toString()),
+                "korespondenci": item.Nadawca + " - P - (123456)",
+                "temat": item.Tytul,
+                "data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "skrzynka": "Jan Kowalski - U - (123456)",
+                "hasZalaczniki": true,
+                "przeczytana": !!item.GodzinaPrzeczytania,
+                "nieprzeczytanePrzeczytanePrzez": null,
+                "wazna": false,
+                "uzytkownikRola": 2,
+                "id": item.WiadomoscId
             };
         })
     });
@@ -115,133 +89,97 @@ router.get([
         "success": true,
         "data": require("../../data/api/messages/WiadomosciUsuniete").map(item => {
             return {
-                "Id": item.WiadomoscId * 2,
-                "Nieprzeczytana": !item.GodzinaPrzeczytania,
-                "Nieprzeczytane": parseInt(item.Nieprzeczytane, 10),
-                "Przeczytane": parseInt(item.Przeczytane, 10),
-                "Data": converter.formatDate(new Date(item.DataWyslaniaUnixEpoch * 1000), true) + ' 00:00:00',
-                "Tresc": null,
-                "Temat": item.Tytul,
-                "Nadawca": {
-                    "Id": "" + item.NadawcaId,
-                    "Name": item.Nadawca,
-                    "IdLogin": item.NadawcaId,
-                    "Unreaded": false,
-                    "Date": null,
-                    "Role": 2,
-                    "PushMessage": false,
-                    "UnitId": 0,
-                    "Hash": "abcdef="
-                },
-                "IdWiadomosci": item.WiadomoscId,
-                "HasZalaczniki": false,
-                "FolderWiadomosci": 3,
-                "Adresaci": []
+                "apiGlobalKey": fromString(item.WiadomoscId.toString()),
+                "korespondenci": item.Nadawca + " - P - (123456)",
+                "temat": item.Tytul,
+                "data": new Date(item.DataWyslaniaUnixEpoch * 1000).toISOString(),
+                "skrzynka": "Jan Kowalski - U - (123456)",
+                "hasZalaczniki": true,
+                "przeczytana": !!item.GodzinaPrzeczytania,
+                "nieprzeczytanePrzeczytanePrzez": null,
+                "wazna": false,
+                "uzytkownikRola": 2,
+                "id": item.WiadomoscId
             };
         })
     });
 });
 
 router.get("/api/Skrzynki", (req, res) => {
-    const user = require("../../data/api/ListaUczniow")[1];
-    res.json({
-        "success": true,
-        "data": [
-            {
-                "IdJednostkaSprawozdawcza": user.IdJednostkaSprawozdawcza,
-                "Skrot": user.JednostkaSprawozdawczaSkrot,
-                "Role": [1],
-                "NazwaNadawcy": user.Imie + " " + user.Nazwisko,
-                "WychowawcaWOddzialach": [],
-                "Id": user.Id
-            }
-        ]
-    });
+    const users = require("../../data/api/ListaUczniow");
+    res.json(users.map(user => {
+        return {
+            "globalKey": fromString(user.UzytkownikLoginId.toString()),
+            "nazwa": `${user.Imie} ${user.Nazwisko} - U - (${user.JednostkaSprawozdawczaSkrot})`,
+            "typUzytkownika": 3
+        };
+    }));
 });
 
 router.all("/api/WiadomoscSzczegoly", (req, res) => {
     const message = require("../../data/api/messages/WiadomosciOdebrane")[0];
     res.json({
-        "success": true,
-        "data": {
-            "Id": message.WiadomoscId,
-            "Tresc": message.Tresc,
-            "Zalaczniki": [
-                {
-                    "Url": "https://1drv.ms/u/s!AmvjLDq5anT2psJ4nujoBUyclWOUhw",
-                    "IdOneDrive": "0123456789ABCDEF!123",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "nazwa_pliku.pptx",
-                    "Id": message.WiadomoscId * 3
-                },
-                {
-                    "Url": "https://wulkanowy.github.io/",
-                    "IdOneDrive": "0123456789ABCDEF!124",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "wulkanowy.txt",
-                    "Id": message.WiadomoscId * 4
-                },
-                {
-                    "Url": "https://github.com/wulkanowy/wulkanowy",
-                    "IdOneDrive": "0123456789ABCDEF!125",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "wulkanowy(2).txt",
-                    "Id": message.WiadomoscId * 5
-                }
-            ]
-        }
+        "data": new Date(message.DataWyslaniaUnixEpoch * 1000).toISOString(),
+        "apiGlobalKey": fromString(message.WiadomoscId.toString()),
+        "nadawca": "Natalia Wrzesień - P - (123456)",
+        "odbiorcy": ["Jan kowalski - U - (123456)"],
+        "temat": message.Tytul,
+        "tresc": message.Tresc.replaceAll("\n", "<br>"),
+        "odczytana": true,
+        "zalaczniki": [
+            {
+                "url": "https://1drv.ms/u/s!AmvjLDq5anT2psJ4nujoBUyclWOUhw",
+                "nazwaPliku": "nazwa_pliku.pptx",
+            },
+            {
+                "url": "https://wulkanowy.github.io/",
+                "nazwaPliku": "wulkanowy.txt",
+            },
+            {
+                "url": "https://github.com/wulkanowy/wulkanowy",
+                "nazwaPliku": "wulkanowy(2).txt",
+            }
+        ],
+        "id": message.WiadomoscId
     });
 });
 
 router.all("/api/WiadomoscOdpowiedzPrzekaz", (req, res) => {
+    const user = require("../../data/api/ListaUczniow")[1];
     const message = require("../../data/api/messages/WiadomosciOdebrane")[0];
     res.json({
-        "success": true,
-        "data": {
-            "Id": message.WiadomoscId,
-            "Tresc": message.Tresc,
-            "Zalaczniki": [
-                {
-                    "Url": "https://1drv.ms/u/s!AmvjLDq5anT2psJ4nujoBUyclWOUhw",
-                    "IdOneDrive": "0123456789ABCDEF!123",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "nazwa_pliku.pptx",
-                    "Id": message.WiadomoscId * 3
-                },
-                {
-                    "Url": "https://wulkanowy.github.io/",
-                    "IdOneDrive": "0123456789ABCDEF!124",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "wulkanowy.txt",
-                    "Id": message.WiadomoscId * 4
-                },
-                {
-                    "Url": "https://github.com/wulkanowy/wulkanowy",
-                    "IdOneDrive": "0123456789ABCDEF!125",
-                    "IdWiadomosc": message.WiadomoscId,
-                    "NazwaPliku": "wulkanowy(2).txt",
-                    "Id": message.WiadomoscId * 5
-                }
-            ]
-        }
+        "data": new Date(message.DataWyslaniaUnixEpoch * 1000).toISOString(),
+        "apiGlobalKey": fromString(message.WiadomoscId.toString()),
+        "uzytkownikSkrzynkaGlobalKey": fromString(user.Id.toString()),
+        "nadawcaSkrzynkaGlobalKey": fromString(message.NadawcaId.toString()),
+        "nadawcaSkrzynkaNazwa": "Natalia Wrzesień - P - (123456)",
+        "adresaci": [
+            {
+                "skrzynkaGlobalKey": fromString(user.Id.toString()),
+                "nazwa": "Jan Kowalski - U - (123456)"
+            }
+        ],
+        "temat": message.Tytul,
+        "tresc": message.Tresc.replaceAll("\n", "<br>"),
+        "zalaczniki": [
+            {
+                "url": "https://1drv.ms/u/s!AmvjLDq5anT2psJ4nujoBUyclWOUhw",
+                "nazwaPliku": "nazwa_pliku.pptx"
+            }
+        ],
+        "id": message.WiadomoscId
     });
 });
 
 router.all("/api/Pracownicy", (req, res) => {
     const user = require("../../data/api/ListaUczniow")[1];
-    const recipient = require("../../data/api/dictionaries/Pracownicy")[1];
-    res.json({
-        "success": true,
-        "data": [
-            {
-                "Id": recipient.Id * 8, // ¯\_(ツ)_/¯
-                "Name": `${recipient.Imie} ${recipient.Nazwisko} [${recipient.Kod}] - pracownik (${user.JednostkaSprawozdawczaSkrot})`,
-                "IdLogin": recipient.LoginId,
-                "Role": 7,
-                "Hash": "abcd==",
-            }
-        ]
-    });
+    const recipients = require("../../data/api/dictionaries/Pracownicy");
+    res.json(recipients.map(item => {
+        return {
+            "skrzynkaGlobalKey": fromString(item.Id.toString()),
+            "nazwa": `${item.Nazwisko} ${item.Imie} - P - (${user.JednostkaSprawozdawczaSkrot})`
+        };
+    }));
 });
 
 router.all([
